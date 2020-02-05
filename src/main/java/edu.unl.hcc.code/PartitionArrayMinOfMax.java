4857944f25408f6c6bce70005957c53a98153778
Chen He <che@netskope.com>
2020-02-04 16:03:59 -0800

    Feb 4th, 2020 commit

---
+++ src/main/java/edu.unl.hcc.code/PartitionArrayMinOfMax.java
@@ -0,0 +1,23 @@
+package edu.unl.hcc.leetcode;
+
+/**
+ * Given a integer array and a number that defines the partitions,
+ * find the min of max value in each partition.
+ *
+ * eg: [1,3,5,0,3,4]  2
+ *
+ * the answer is: 1
+ *
+ */
+public class PartitionArrayMinOfMax {
+
+    public static int find(int[] array, int partitions) {
+
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] test1= new int[]{1,3,5,0,3,4};
+        System.out.println("Actual result should be: " + find(test1, 2));
+    }
+}
